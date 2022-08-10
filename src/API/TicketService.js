a27a267b90861833536c3412d8881542a3393f40
KorzhovVD <svalesik@mail.ru>
2022-08-10 12:51:42 +0300

    corrected url

--- src/API/TicketService.js
+++ src/API/TicketService.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 export default class TicketService {
 	static async getAll () {
-		const response = await axios.get('http://myjson.dit.upm.es/api/bins/gbt6')
+		const response = await axios.get('https://myjson.dit.upm.es/api/bins/gbt6')
 		const priceSort = response.data.sort((a,b) => a["price"] - b["price"])
 		return priceSort
 	}
